29c5153ea9ada24afcae5b6f5584240ec82e769a
windstormer <chenjuzen@gmail.com>
2016-06-20 13:28:54 +0800

    put wheel image

--- app/src/main/java/com/software/studio/delicacies/fragments/Wheel.java
+++ app/src/main/java/com/software/studio/delicacies/fragments/Wheel.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.software.studio.delicacies.R;
 
@@ -12,7 +13,9 @@ public class Wheel extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_wheel, container, false);
+        View view = inflater.inflate(R.layout.fragment_wheel, container, false);
+        ImageView wheel = (ImageView)view.findViewById(R.id.img_wheels);
+        wheel.setImageResource(R.drawable.wheel);
+        return view;
     }
 }
