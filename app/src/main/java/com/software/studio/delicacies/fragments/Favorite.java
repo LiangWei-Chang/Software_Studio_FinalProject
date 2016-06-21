4016ed429bf4fa39b895ae7a51d79a8de992b68e
windstormer <chenjuzen@gmail.com>
2016-06-22 01:54:38 +0800

    modified favorite to recyclerview

--- app/src/main/java/com/software/studio/delicacies/fragments/Favorite.java
+++ app/src/main/java/com/software/studio/delicacies/fragments/Favorite.java
@@ -3,6 +3,8 @@ package com.software.studio.delicacies.fragments;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,35 +12,38 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
+import com.software.studio.delicacies.DetailActivity;
 import com.software.studio.delicacies.MapsActivity;
 import com.software.studio.delicacies.R;
+import com.software.studio.delicacies.RecycleViewAdapter;
 
-public class Favorite extends Fragment implements View.OnClickListener{
-    ImageButton button;
-    EditText editText;
+import java.util.ArrayList;
+
+public class Favorite extends Fragment{
+    RecyclerView.LayoutManager mLayoutManager;
+    RecycleViewAdapter mAdapter;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container ,Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
-        button = (ImageButton) view.findViewById(R.id.image_button_favorite);
-        editText = (EditText) view.findViewById(R.id.favorite_edittext);
-        editText.setHint("  Please type keyword add to favorite");
-        editText.setOnClickListener(this);
-        button.setOnClickListener(this);
-        return view;
+        ArrayList<String> data = new ArrayList<String>();
+        View rootview = inflater.inflate(R.layout.fragment_favorite, container, false);
+        RecyclerView recyclerView = (RecyclerView) rootview.findViewById(R.id.favorite_recycler_view);
+
+        mLayoutManager = new LinearLayoutManager(container.getContext());
+        recyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new RecycleViewAdapter(data);
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setOnItemClickListener(new RecycleViewAdapter.OnRecyclerViewItemClickListener() {
+            @Override
+            public void onItemClick(View view, String data) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), DetailActivity.class);
+                intent.putExtra("name", data);
+                startActivity(intent);
+            }
+        });
+        return rootview;
     }
 
-    public void onClick(View v) {
-        if(v == editText) {
-            editText.getText().clear();
-        }
-        else if(v == button) {
-            if(editText.getText().length()!=0)
-            {
-                Toast.makeText(getActivity(), editText.getText().toString(),Toast.LENGTH_SHORT).show();
-            }
-            else{
-                Toast.makeText(getActivity(), R.string.msg_InputIsEmpty, Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+
+
 }
