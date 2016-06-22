4724690cb7432a8420ed879490f56fea910d5702
lwchang <pinyo9611@gmail.com>
2016-06-22 13:42:34 +0800

    Setting Menu done

--- app/src/main/java/com/software/studio/delicacies/fragments/Settings.java
+++ app/src/main/java/com/software/studio/delicacies/fragments/Settings.java
@@ -1,7 +1,9 @@
 package com.software.studio.delicacies.fragments;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.ContextMenu;
@@ -21,10 +23,11 @@ public class Settings extends Fragment {
     RecyclerView recyclerView;
     RecycleViewAdapter mAdapter;
     View menuView;
-
+    View FragView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootview = inflater.inflate(R.layout.fragment_settings, container, false);
+        FragView = inflater.inflate(R.layout.activity_framement_tags, container, false);
         RecyclerView.LayoutManager mLayoutManager;
         ArrayList<String> data = new ArrayList<String>();
         data.add("Notifications");
@@ -32,7 +35,6 @@ public class Settings extends Fragment {
         data.add("Password");
         data.add("Reset");
         data.add("About");
-
         recyclerView = (RecyclerView) rootview.findViewById(R.id.setting_recycler_view);
         mLayoutManager = new LinearLayoutManager(container.getContext());
         recyclerView.setLayoutManager(mLayoutManager);
@@ -47,10 +49,33 @@ public class Settings extends Fragment {
                     recyclerView.showContextMenuForChild(view);
                 }
                 else if(data == mAdapter.getDataset().get(3)){
-
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("Are you sure to reset the datas ?");
+                    builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //TODO
+                            dialog.dismiss();
+                        }
+                    });
+                    builder.setNegativeButton(android.R.string.cancel, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            //TODO
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
                 else if(data == mAdapter.getDataset().get(4)){
-
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                    builder.setTitle("Class: Software Studio\nAuthor: Liang-Wei Chang, Ju-Zen Chen");
+                    builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.dismiss();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
                 }
             }
         });
@@ -87,35 +112,35 @@ public class Settings extends Fragment {
         switch (item.getGroupId()){
             case 0:
                 if(item.getTitle().equals("On")){
-                    Toast.makeText(getContext(), R.string.msg_NotificationOn, Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_NotificationOn, Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Off")){
-                    Toast.makeText(getContext(), R.string.msg_NotificationOff, Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_NotificationOff, Toast.LENGTH_SHORT).show();
                 }
                 break;
             case 1:
                 if(item.getTitle().equals("Red")){
-                    Toast.makeText(getContext(), R.string.msg_ChangeColor + item.getTitle().toString(), Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_ChangeColor, Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Blue")){
-                    Toast.makeText(getContext(), R.string.msg_ChangeColor + item.getTitle().toString(), Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_ChangeColor, Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Green")){
-                    Toast.makeText(getContext(), R.string.msg_ChangeColor + item.getTitle().toString(), Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_ChangeColor, Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Orange")){
-                    Toast.makeText(getContext(), R.string.msg_ChangeColor + item.getTitle().toString(), Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_ChangeColor, Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Purple")){
-                    Toast.makeText(getContext(), R.string.msg_ChangeColor + item.getTitle().toString(), Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), R.string.msg_ChangeColor, Toast.LENGTH_SHORT).show();
                 }
                 break;
             case 2:
                 if(item.getTitle().equals("On")){
-                    Toast.makeText(getContext(), "Password On", Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), "Password On", Toast.LENGTH_SHORT).show();
                 }
                 else if(item.getTitle().equals("Off")){
-                    Toast.makeText(getContext(), "Password Off", Toast.LENGTH_SHORT);
+                    Toast.makeText(getContext(), "Password Off", Toast.LENGTH_SHORT).show();
                 }
                 break;
         }
